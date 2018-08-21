79f36e95dd49f12a1743f1bcd47c66ce473a6140
Magda Kłopotek <magda.klopotek@gmail.com>
2018-08-21 10:16:17 +0200

    loading material-ui added

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
 
 const Loading = () => (
-    <div>Loading...</div>
+    <div>
+        <CircularProgress
+            size={60}
+            thickness={7}
+        />
+    </div>
 )
 
 export default Loading
